00d01e176cd506c4006ed67201584d9629c45846
EnigmaOppo <guconglin@oppo.com>
2021-12-23 20:42:55 +0800

    feat(extension inject): add copyright info and fix the error of checkstyle

--- commons/src/main/java/esa/commons/spi/SpiLoader.java
+++ commons/src/main/java/esa/commons/spi/SpiLoader.java
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2020 OPPO ESA Stack Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package esa.commons.spi;
 
 import esa.commons.Checks;
@@ -635,9 +649,9 @@ public class SpiLoader<T> {
                                 return null;
                             } else {
                                 throw new RuntimeException("The dependencies of some of the beans in the application " +
-                                        "context form a cycle, one of the bean is " + pair.getName() + ", As a last resort, " +
-                                        "it may be possible to break the cycle automatically by setting " +
-                                        "env allow.circular.references to true or " +
+                                        "context form a cycle, one of the bean is " + pair.getName() +
+                                        ", As a last resort, it may be possible to break the cycle automatically " +
+                                        "by setting env allow.circular.references to true or " +
                                         "setting VM options -Dallow.circular.references to true.");
                             }
                         } else {
--- commons/src/test/java/esa/commons/spi/extensionloader/inject/ConstructorInjectBean1.java
+++ commons/src/test/java/esa/commons/spi/extensionloader/inject/ConstructorInjectBean1.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 OPPO ESA Stack Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package esa.commons.spi.extensionloader.inject;
 
 import esa.commons.spi.factory.Inject;
@@ -9,7 +24,8 @@ public class ConstructorInjectBean1 implements ConstructorInjectBean {
     private Integer salary;
     private ConstructorInjectBean2 bean2;
 
-    public ConstructorInjectBean1() {}
+    public ConstructorInjectBean1() {
+    }
 
     @Inject(name = "bean2")
     public ConstructorInjectBean1(int age, String name, Integer salary, ConstructorInjectBean2 bean2) {
